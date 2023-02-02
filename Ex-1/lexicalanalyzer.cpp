edc5a3775a2e524d1faed30ecc69e938a48903c5
Vikash2806 <vikashrajendran2806@gmail.com>
2023-02-02 05:43:54 +0530

    refactor: lexical analyzer code

--- Ex-1/lexicalanalyzer.cpp
+++ Ex-1/lexicalanalyzer.cpp
@@ -1,95 +1,94 @@
-#include<iostream>
-#include<cstring>
-#include<stdlib.h>
-#include<ctype.h>
+#include <iostream>
+#include <cstring>
+#include <stdlib.h>
+#include <ctype.h>
 #include <fstream>
 #include <string>
 using namespace std;
 
-string arr[] = { "void", "using", "namespace", "int", "include", "iostream", "std", "main",
-"cin", "cout", "return", "float", "double", "string" };
+string arr[] = {"void", "using", "namespace", "int", "include", "iostream", "std", "main",
+                "cin", "cout", "return", "float", "double", "string"};
 
-bool isKeyword (string a)
+bool isKeyword(string a)
 {
-for (int i = 0; i < 14; i++)
-{
-if (arr[i] == a)
+    for (int i = 0; i < 14; i++)
+    {
+        if (arr[i] == a)
 
-{
-return true;
-}
-}
-return false;
-}
-
-int main ()
-{
-
-std::ifstream file("prog.txt");
-std::string x;
-string code="";
-while (std::getline(file, x)) {
-code+=x;
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
-string s="";
-for (int i = 0; i < code.size (); i++)
+int main()
 {
 
-if (code[i] != ' ')
-s += code[i];
+    std::ifstream file("prog.txt");
+    std::string x;
+    string code = "";
+    while (std::getline(file, x))
+    {
+        code += x;
+    }
 
-else
-{
-if (s == "+" || s == "-" || s == "*" || s == "/" || s == "^" || s == "&&" || s == "||" ||
-s == "=" || s == "==" || s == "&" || s == "|" || s == "%" || s == "++" || s == "--" || s
-== "+=" ||
-s == "-=" || s == "/=" || s == "*=" || s == "%=")
+    string s = "";
+    for (int i = 0; i < code.size(); i++)
+    {
 
-{
-cout << s <<" is an operator"<<endl;
-s = "";
-}
+        if (code[i] != ' ')
+            s += code[i];
 
-else if (isKeyword (s))
-{
-cout << s <<" is a keyword"<<endl;
-s = "";
-}
+        else
+        {
+            if (s == "+" || s == "-" || s == "*" || s == "/" || s == "^" || s == "&&" || s == "||" ||
+                s == "=" || s == "==" || s == "&" || s == "|" || s == "%" || s == "++" || s == "--" || s == "+=" ||
+                s == "-=" || s == "/=" || s == "*=" || s == "%=")
 
-else if (s == "(" || s == "{" || s == "[" || s == ")" || s == "}" || s == "]" || s == "<" ||
-s == ">" || s == "()" || s == ";" || s == "<<" || s == ">>" || s == "," || s == "#")
-{
-cout << s <<" is a symbol"<<endl;
-s = "";
-}
+            {
+                cout << s << " is an operator" << endl;
+                s = "";
+            }
 
-else if (s == "\n" || s == "" || s == "")
-{
-s = "";
-}
+            else if (isKeyword(s))
+            {
+                cout << s << " is a keyword" << endl;
+                s = "";
+            }
 
-else if (isdigit (s[0]))
-{
+            else if (s == "(" || s == "{" || s == "[" || s == ")" || s == "}" || s == "]" || s == "<" ||
+                     s == ">" || s == "()" || s == ";" || s == "<<" || s == ">>" || s == "," || s == "#")
+            {
+                cout << s << " is a symbol" << endl;
+                s = "";
+            }
 
-int x = 0;
-if (!isdigit (s[x++]))
-{
-continue;
-}
-else
-{
-cout << s <<" is a constant"<<endl;
-s = "";
-}
-}
+            else if (s == "\n" || s == "" || s == "")
+            {
+                s = "";
+            }
 
-else
-{
-cout << s <<" is an identifier"<<endl;
-s = "";
-}
+            else if (isdigit(s[0]))
+            {
 
-}
-}
+                int x = 0;
+                if (!isdigit(s[x++]))
+                {
+                    continue;
+                }
+                else
+                {
+                    cout << s << " is a constant" << endl;
+                    s = "";
+                }
+            }
+
+            else
+            {
+                cout << s << " is an identifier" << endl;
+                s = "";
+            }
+        }
+    }
 }
